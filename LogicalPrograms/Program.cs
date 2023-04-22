636cb8d6a89fa592e428ef631a7df60ad8afdd64
“riteshwaykole21” <riteshwaykole21@gmail.com>
2023-04-22 12:52:17 +0530

    [Ritesh]add : Swapping Tweo Numbers

---
+++ LogicalPrograms/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1 : Swap Of Two Numbers");
+            Console.WriteLine("Enter A Option");
+            int Option = Convert.ToInt32(Console.ReadLine());
+            switch(Option)
+            {
+                case 1:
+                    SwapNumbers swapNumbers = new SwapNumbers();
+                    swapNumbers.Numbers();
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ LogicalPrograms/SwapNumbers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    public class SwapNumbers
+    {
+        public void Numbers()
+        {
+            Console.WriteLine("Enter First Number");
+            int FirstNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Second Number ");
+            int SecondNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($" After Swapping The First Number is {SecondNumber} ");
+            Console.WriteLine($" After Swapping The Second Number is {FirstNumber} ");
+        }
+    }
+}
